573c9c0177f1dcef5a8c08e2cb51a743168b6f1f
darithorn <aeronavery42@gmail.com>
2018-05-29 11:47:34 -0600

    remove linking lib

--- build.zig
+++ build.zig
@@ -4,10 +4,10 @@ const Version = @import("std").build.Version;
 pub fn build(builder: &Builder) void {
     builder.addLibPath("/usr/lib64/");
     builder.addCIncludePath("./godot_headers/");
-    var exe = builder.addSharedLibrary("godotzig", "src/main.zig", builder.version(0, 0, 1));
+    var exe = builder.addSharedLibrary("godotzig", "godot/core/index.zig", builder.version(0, 0, 1));
     exe.setBuildMode(builder.standardReleaseOptions());
 
-    exe.linkSystemLibrary("c");
+    // exe.linkSystemLibrary("c");
 
     builder.default_step.dependOn(&exe.step);
     builder.installArtifact(exe);
