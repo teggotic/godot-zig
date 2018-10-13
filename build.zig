48866c8994353aa5ecad57d18ed0a0778172051a
darithorn <aeronavery42@gmail.com>
2018-10-12 21:09:33 -0600

    added testing step in build script

--- build.zig
+++ build.zig
@@ -1,7 +1,7 @@
 const Builder = @import("std").build.Builder;
 const Version = @import("std").build.Version;
 
-pub fn build(builder: &Builder) void {
+pub fn build(builder: *Builder) void {
     builder.addLibPath("/usr/lib64/");
     builder.addCIncludePath("./godot_headers/");
     var exe = builder.addSharedLibrary("godotzig", "godot/core/index.zig", builder.version(0, 0, 1));
@@ -11,4 +11,9 @@ pub fn build(builder: &Builder) void {
 
     builder.default_step.dependOn(&exe.step);
     builder.installArtifact(exe);
+
+    const test_step = builder.step("test", "Test");
+    const build_test = builder.addTest("godot/core/tests.zig");
+    build_test.linkSystemLibrary("c");
+    test_step.dependOn(&build_test.step);
 }
